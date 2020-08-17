1d3ef414ce1e7e6591b7dbd4696bdc47402c4105
Timothy Harris <timothy.harris@eficode.com>
2020-08-17 15:12:00 +0200

    Add the step client as part of traefik image.
    This way we don't need to mount certs from host.

---
+++ demos/traefik/wrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+step ca bootstrap --ca-url "${CERT_AUTH}:${CERT_AUTH_PORT}" --fingerprint "${FINGERPRINT}" --install --force
+
+unset "FINGERPRINT"
+
+update-ca-certificates
+
+# Todo: parse and check ok else exit...
+curl -sS "${CERT_AUTH}:${CERT_AUTH_PORT}/health"
+
+# Just pass all commands to original entyrpoint.sh script
+./entrypoint.sh "$@"
